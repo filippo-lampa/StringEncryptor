6eab11e56c8242eb3f0100888565b6cb0596172e
Filippo Lampa <56674164+filippo-lampa@users.noreply.github.com>
2021-04-17 01:00:43 +0200

    Update CryptoChar.java

--- src/main/java/CryptoChar.java
+++ src/main/java/CryptoChar.java
@@ -1,11 +1,18 @@
 
 public class CryptoChar {
 
-    int codeLength;
-    boolean isDivider;
+    private int codeLength;
+    private boolean isDivider;
 
     public CryptoChar(int codeLength, boolean isDivider){
         this.codeLength = codeLength;
         this.isDivider = isDivider;
+    }
+    public int getCodeLength(){
+        return codeLength;
+    }
+
+    public boolean getIsDivider(){
+        return isDivider;
     }
 }
